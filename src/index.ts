06629b843315d84e4ae3304013c5f919abadfd72
Sebastian Koch <s.koch@alexander-buerkle.de>
2023-08-09 15:00:42 +0200

    try something

--- src/index.ts
+++ src/index.ts
@@ -9,38 +9,10 @@ import {
   Resolver,
   buildSchema,
 } from "type-graphql";
-
+import { TestResolver } from "./resolver/testResolver";
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-
-@ObjectType()
-class Recipe {
-  @Field((type) => String)
-  id: string;
-}
-
-@Resolver()
-class Resolver1 {
-  constructor() {}
-
-  @Query((returns) => Recipe)
-  async recipe(@Arg("id") id?: string) {
-    console.log("id", id);
-
-    return id;
-  }
-}
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
 
 export type MyContext = {
   // You can optionally create a TS interface to set up types
@@ -50,11 +22,13 @@ export type MyContext = {
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const schema = await buildSchema({ resolvers: [Resolver1] });
+// const schema = await buildSchema({ resolvers: [Resolver1] });
+const schema = await buildSchema({
+  resolvers: [TestResolver],
+});
 const server = new ApolloServer<MyContext>({
   schema,
 });
-
 // Passing an ApolloServer instance to the `startStandaloneServer` function:
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
---
+++ src/resolver/testResolver.ts
@@ -0,0 +1,19 @@
+import { Arg, Field, ObjectType, Query, Resolver } from "type-graphql";
+
+@ObjectType()
+class Recipe {
+  @Field((type) => String)
+  id: string;
+}
+
+@Resolver()
+export class TestResolver {
+  constructor() {}
+
+  @Query((returns) => Recipe)
+  async recipe(@Arg("id") id?: string) {
+    console.log("id", id);
+
+    return id;
+  }
+}
