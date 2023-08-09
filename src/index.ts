23a8b922cc3e9c504365494b40d682f798502dbf
Sebastian Koch <s.koch@alexander-buerkle.de>
2023-08-09 08:11:48 +0200

    trying 2.0 beta

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,36 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { readFileSync } from "fs";
+import "reflect-metadata";
+import {
+  Arg,
+  Field,
+  ObjectType,
+  Query,
+  Resolver,
+  buildSchema,
+} from "type-graphql";
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
+@ObjectType()
+class Recipe {
+  @Field((type) => String)
+  id: string;
+}
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
+@Resolver()
+class Resolver1 {
+  constructor() {}
+
+  @Query((returns) => Recipe)
+  async recipe(@Arg("id") id?: string) {
+    console.log("id", id);
+
+    return id;
   }
-`;
-const newTypeDefs = readFileSync("./schema.graphql", { encoding: "utf-8" });
+}
 const books = [
   {
     title: "The Awakening",
@@ -33,14 +42,6 @@ const books = [
   },
 ];
 
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: (_, __, contextValue) => books,
-  },
-};
-
 export type MyContext = {
   // You can optionally create a TS interface to set up types
   // for your contextValue
@@ -49,9 +50,9 @@ export type MyContext = {
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
+const schema = await buildSchema({ resolvers: [Resolver1] });
 const server = new ApolloServer<MyContext>({
-  typeDefs: newTypeDefs,
-  resolvers,
+  schema,
 });
 
 // Passing an ApolloServer instance to the `startStandaloneServer` function:
